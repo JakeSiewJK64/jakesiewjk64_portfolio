ea694462a7192a01083274fbea7bdfaac7b85177
JakeSiewJK64 <joekanesiew@outlook.com>
2024-09-07 11:28:43 +0800

    fix height of paeg

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -4,7 +4,7 @@ import ProfileCard from "@/components/ProfileCard";
 const Home = () => {
   return (
     <div className="flex h-[100vh]">
-      <div className="max-w-[80rem] mx-[auto] gap-4 flex-row flex mt-[auto] mb-[auto]">
+      <div className="h-[25rem] max-w-[80rem] mx-[auto] gap-4 flex-row flex mt-[auto] mb-[auto]">
         <ProfileCard />
         <ProfileBody />
       </div>
