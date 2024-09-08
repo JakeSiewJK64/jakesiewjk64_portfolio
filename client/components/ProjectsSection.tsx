5f0aec3087afdcecfed43612bd3fcdf821856fd8
JakeSiewJK64 <joekanesiew@outlook.com>
2024-09-08 09:10:56 +0800

    add fallback for work projects

--- client/components/ProjectsSection.tsx
+++ client/components/ProjectsSection.tsx
@@ -6,43 +6,45 @@ import { Badge } from "./ui/badge";
 const ProjectsSection = async () => {
   const projects = await useFetchPersonalProjects();
 
-  if (!projects) {
-    return null;
-  }
-
   return (
     <div
       id="project"
       className="border border-lightgray-500 rounded-sm p-6 mb-4"
     >
       <p className="text-xl font-bold">Projects</p>
-      {projects.map((project) => (
-        <div className="flex flex-col my-1 py-2" key={project.title}>
-          <p>
-            <strong>Title: </strong>
-            {project.title}
-          </p>
-          <p>
-            <strong>Date: </strong>
-            {project.date}
-          </p>
-          <div className="flex flex-row gap-2 flex-wrap">
-            {project.tags.map((tag) => (
-              <Badge key={tag}>{tag}</Badge>
-            ))}
-          </div>
-          <Link
-            className={buttonVariants({
-              variant: "link",
-              size: "sm",
-              className: "w-[8rem] ml-[auto]",
-            })}
-            href={project.url}
-          >
-            View Project
-          </Link>
-        </div>
-      ))}
+      {!projects ? (
+        <p>No data detected. Please ensure your data source is valid.</p>
+      ) : (
+        <>
+          {projects.map((project) => (
+            <div className="flex flex-col my-1 py-2" key={project.title}>
+              <p>
+                <strong>Title: </strong>
+                {project.title}
+              </p>
+              <p>
+                <strong>Date: </strong>
+                {project.date}
+              </p>
+              <div className="flex flex-row gap-2 flex-wrap">
+                {project.tags.map((tag) => (
+                  <Badge key={tag}>{tag}</Badge>
+                ))}
+              </div>
+              <Link
+                className={buttonVariants({
+                  variant: "link",
+                  size: "sm",
+                  className: "w-[8rem] ml-[auto]",
+                })}
+                href={project.url}
+              >
+                View Project
+              </Link>
+            </div>
+          ))}
+        </>
+      )}
     </div>
   );
 };
