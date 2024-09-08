75970422f4ce8ed09baab185625794f334848385
JakeSiewJK64 <joekanesiew@outlook.com>
2024-09-08 09:11:26 +0800

    add fallback for work education

--- client/components/EducationSection.tsx
+++ client/components/EducationSection.tsx
@@ -5,36 +5,38 @@ import { buttonVariants } from "./ui/button";
 const EducationSection = async () => {
   const education = await useFetchEducation();
 
-  if (!education) {
-    return null;
-  }
-
   return (
-    <div id="education" className="mb-4 border border-lightgray-500 rounded-sm p-6">
+    <div
+      id="education"
+      className="mb-4 border border-lightgray-500 rounded-sm p-6"
+    >
       <p className="text-xl font-bold">Education</p>
-      {education.map((item) => (
-        <div
-          className="flex flex-col my-1 p-2"
-          key={item.title}
-        >
-          <div className="flex flex-row justify-between">
-            <p>{item.title}</p>
-            <Link
-              className={buttonVariants({
-                variant: "link",
-                size: "sm",
-                className: "ml-[auto]",
-              })}
-              href={item.link}
-            >
-              Link
-            </Link>
-          </div>
-          <p>
-            {item.institute}, {item.location}
-          </p>
-        </div>
-      ))}
+      {!education ? (
+        <p>No data detected. Please ensure your data source is valid.</p>
+      ) : (
+        <>
+          {education.map((item) => (
+            <div className="flex flex-col my-1 p-2" key={item.title}>
+              <div className="flex flex-row justify-between">
+                <p>{item.title}</p>
+                <Link
+                  className={buttonVariants({
+                    variant: "link",
+                    size: "sm",
+                    className: "ml-[auto]",
+                  })}
+                  href={item.link}
+                >
+                  Link
+                </Link>
+              </div>
+              <p>
+                {item.institute}, {item.location}
+              </p>
+            </div>
+          ))}
+        </>
+      )}
     </div>
   );
 };
