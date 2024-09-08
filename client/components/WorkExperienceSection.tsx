9e4baa2ab9db793810cf827c04c0a9712fe42ebc
JakeSiewJK64 <joekanesiew@outlook.com>
2024-09-08 09:10:10 +0800

    add fallback for work experience

--- client/components/WorkExperienceSection.tsx
+++ client/components/WorkExperienceSection.tsx
@@ -13,34 +13,40 @@ import { Badge } from "./ui/badge";
 const WorkExperienceSection = async () => {
   const experiences = await useFetchWorkExperience();
 
-  if (!experiences) return null;
-
   return (
     <div
       id="work_experience"
       className="border border-lightgray-500 rounded-sm p-6 mb-4"
     >
       <p className="text-xl font-bold">Work Experience</p>
-      {experiences.map((experience) => (
-        <Timeline key={experience.start_date}>
-          <TimelineItem>
-            <TimelineConnector />
-            <TimelineHeader>
-              <TimelineIcon />
-              <TimelineTitle>{experience.company}</TimelineTitle>
-            </TimelineHeader>
-            <TimelineContent>
-              <p>{experience.start_date}</p>
-              <div className="flex flex-row gap-2 flex-wrap">
-                {experience.tags.map((tag) => (
-                  <Badge key={tag}>{tag}</Badge>
-                ))}
-              </div>
-              <p>{experience.description}</p>
-            </TimelineContent>
-          </TimelineItem>
-        </Timeline>
-      ))}
+      {!experiences ? (
+        <p>
+          No data detected. Please ensure your data source is valid.
+        </p>
+      ) : (
+        <>
+          {experiences.map((experience) => (
+            <Timeline key={experience.start_date}>
+              <TimelineItem>
+                <TimelineConnector />
+                <TimelineHeader>
+                  <TimelineIcon />
+                  <TimelineTitle>{experience.company}</TimelineTitle>
+                </TimelineHeader>
+                <TimelineContent>
+                  <p>{experience.start_date}</p>
+                  <div className="flex flex-row gap-2 flex-wrap">
+                    {experience.tags.map((tag) => (
+                      <Badge key={tag}>{tag}</Badge>
+                    ))}
+                  </div>
+                  <p>{experience.description}</p>
+                </TimelineContent>
+              </TimelineItem>
+            </Timeline>
+          ))}
+        </>
+      )}
     </div>
   );
 };
