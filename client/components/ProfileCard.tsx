020be89d7fda035b724b745cc4d8fc8130a56b86
JakeSiewJK64 <joekanesiew@outlook.com>
2024-09-08 09:08:31 +0800

    add fallback if no data available

--- client/components/ProfileCard.tsx
+++ client/components/ProfileCard.tsx
@@ -12,41 +12,45 @@ import QrComponent from "./ProfileURLQR";
 const ProfileCard = async () => {
   const profile = await useFetchGithubProfile();
 
-  if (!profile) {
-    return null;
-  }
-
   return (
     <div className="border border-lightgray-500 p-6 rounded-sm flex flex-col items-center">
       <div className="w-14 mb-4 h-2 bg-black rounded-sm" />
-      <Image
-        src={profile.avatar_url}
-        width={120}
-        height={120}
-        className="rounded-[50%]"
-        alt="profile"
-      />
-      <QrComponent />
-      <div className="text-center">
-        <p className="text-xl font-bold uppercase">{profile.name}</p>
-        <p>{profile.login}</p>
-      </div>
-      <div className="flex flex-row gap-2 mt-4 justify-center">
-        <CopyToClipboard icon={<MailIcon />} value={profile.email} />
-        <Link
-          className={buttonVariants({ variant: "outline", size: "icon" })}
-          href={profile.html_url}
-        >
-          <BsGithub size={20} />
-        </Link>
-        <Link
-          className={buttonVariants({ variant: "outline", size: "icon" })}
-          href={profile.blog}
-        >
-          <LiaLinkedin size={20} />
-        </Link>
-      </div>
-      <BarcodeComponent value={String(profile.id)} />
+      {!profile ? (
+        <p className="my-auto max-w-[10rem] text-center">
+          No data detected. Please ensure your data source is valid.
+        </p>
+      ) : (
+        <>
+          <Image
+            src={profile.avatar_url}
+            width={120}
+            height={120}
+            className="rounded-[50%]"
+            alt="profile"
+          />
+          <QrComponent />
+          <div className="text-center">
+            <p className="text-xl font-bold uppercase">{profile.name}</p>
+            <p>{profile.login}</p>
+          </div>
+          <div className="flex flex-row gap-2 mt-4 justify-center">
+            <CopyToClipboard icon={<MailIcon />} value={profile.email} />
+            <Link
+              className={buttonVariants({ variant: "outline", size: "icon" })}
+              href={profile.html_url}
+            >
+              <BsGithub size={20} />
+            </Link>
+            <Link
+              className={buttonVariants({ variant: "outline", size: "icon" })}
+              href={profile.blog}
+            >
+              <LiaLinkedin size={20} />
+            </Link>
+          </div>
+          <BarcodeComponent value={String(profile.id)} />
+        </>
+      )}
     </div>
   );
 };
